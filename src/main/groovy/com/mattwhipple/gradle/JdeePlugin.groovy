adba3506e1936410a4112d75b24758a2b66b8ed2
Matt Whipple <matt@mattwhipple.com>
2016-04-11 08:49:10 -0400

    Prepare for 0.1.0 release

--- src/main/groovy/com/mattwhipple/gradle/JdeePlugin.groovy
+++ src/main/groovy/com/mattwhipple/gradle/JdeePlugin.groovy
@@ -1,43 +1,42 @@
 package com.mattwhipple.gradle
 
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+import org.gradle.api.*
 
 class JdeePlugin implements Plugin<Project> {
 
   def prj = { project ->
 
     "(jdee-project-file-version" (["1.0"])
-    "(jdee-set-variables" { 
+    "(jdee-set-variables" {
       "'(jdee-compile-option-directory" ([project.sourceSets.main.output.classesDir])
       "'(jdee-junit-working-directory" ([project.projectDir])
-    
-      "'(jdee-compile-option-source" { 
-	"'(" (["default"])
+
+      "'(jdee-compile-option-source" {
+        "'(" (["default"])
       }
 
-      "'(jdee-compile-option-target" { 
-	"'(" (["default"])
+      "'(jdee-compile-option-target" {
+        "'(" (["default"])
       }
 
-      "'(jdee-compile-option-command-line-args" { 
-	"'(" (["-${project.sourceCompatibility}"])
+      "'(jdee-compile-option-command-line-args" {
+        "'(" (["-${project.sourceCompatibility}"])
       }
-    
-      "'(jdee-sourcepath" { 
-	"'(" (
-	  project.sourceSets.main.allSource.srcDirs 
-	  + project.sourceSets.test.allSource.srcDirs)
+
+      "'(jdee-sourcepath" {
+        "'(" (
+            project.sourceSets.main.allSource.srcDirs
+            + project.sourceSets.test.allSource.srcDirs)
       }
-    
-      "'(jdee-global-classpath" { 
-	"'(" (
-	  [] + project.sourceSets.main.output.classesDir
-	  + project.sourceSets.test.output.classesDir
-	  + project.sourceSets.main.allSource.srcDirs
-	  + project.sourceSets.test.allSource.srcDirs
-	  + (([] as Set) + project.configurations.compile.getFiles() 
-	     + project.configurations.testCompile.getFiles()))
+
+      "'(jdee-global-classpath" {
+        "'(" (
+            [] + project.sourceSets.main.output.classesDir
+            + project.sourceSets.test.output.classesDir
+            + project.sourceSets.main.allSource.srcDirs
+            + project.sourceSets.test.allSource.srcDirs
+            + (([] as Set) + project.configurations.compile.getFiles()
+               + project.configurations.testCompile.getFiles()))
       }
     }
   }
@@ -45,37 +44,37 @@ class JdeePlugin implements Plugin<Project> {
   @Override
   void apply(Project project) {
 
-    project.task("jdee") << { 
+    project.task("jdee") << {
       def output = new File(project.projectDir, "prj.el").newPrintWriter()
-      try { 
+      try {
 
-	prj.delegate = new NodeBuilder() { 
-	  def lev = 0
-          
-	  def write = { Object file -> 
-	    output.print '\n' + ''.padRight(lev, ' ') +  "\"${file}\"".tr('\\', '/') 
-	  }
+          prj.delegate = new NodeBuilder() {
+          def lev = 0
 
-	  Object createNode(Object name) { 
-	    output.print '\n'  + ''.padRight(lev++, ' ') + name
-	    return name
-	  }
-  
-	  Object createNode(Object name, Object value) { 
-	    createNode(name)
-	    value.each write
-	    return name
-	  }
+          def write = { Object file ->
+              output.print '\n' + ''.padRight(lev, ' ') +  "\"${file}\"".tr('\\', '/') 
+          }
 
-	  void nodeCompleted(Object parent, Object child) { 
-	    output.print ")"
-	    lev--
-	  }
-	}
-	prj(project)
-	output.close()
-      } finally { 
-	output.flush()
+          Object createNode(Object name) {
+              output.print '\n'  + ''.padRight(lev++, ' ') + name
+              return name
+          }
+
+          Object createNode(Object name, Object value) {
+              createNode(name)
+              value.each write
+              return name
+          }
+
+          void nodeCompleted(Object parent, Object child) {
+              output.print ")"
+              lev--
+          }
+        }
+        prj(project)
+        output.close()
+      } finally {
+          output.flush()
       }
     }
   }
